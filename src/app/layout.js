8a68b65030e3bdb9f3f5b38872ce8daa27924153
Michael Mount <michaeljoesphmount@gmail.com>
2026-04-24 16:23:45 -0400

    add Speed Insight plugin

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import NavBar from "./components/layout/Header/NavBar";
 import Footer from "./components/layout/Footer/Footer";
 
